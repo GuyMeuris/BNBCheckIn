60b8ae404abe2d7a3e24e2f05fb6f94288104f9d
Guy Meuris <guy@vises.be>
2021-05-16 18:28:57 +0200

    Added Generic Repository Pattern Added Unit Of Work Pattern

---
+++ Business/Repository/GenericRepository.cs
@@ -0,0 +1,96 @@
+﻿using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Business.Repository.IRepository.IGenericRepository;
+
+namespace Business.Repository
+{
+    // This generic class inherits from the generic interface that implements
+    // some basic 'CRUD'-operations.
+
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly BnBDbContext _context;
+
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(BnBDbContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+
+        //*****************************************************************************************
+
+        // Here we will provide a body to the function-headers that were implemented 
+        // by inheriting from the interface.
+
+        public async Task Delete(int id)
+        {
+            var entity = await _db.FindAsync(id);
+            _db.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _db.RemoveRange(entities);
+        }
+
+        public async Task<T> Get(System.Linq.Expressions.Expression<System.Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+
+            if (includes is not null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<T>> GetAll(System.Linq.Expressions.Expression<System.Func<T, bool>> expression = null, System.Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+
+            if (expression is not null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes is not null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy is not null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task Create(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+
+        }
+
+    }
+}
---
+++ Business/UnitOfWorkPattern/IUnitOfWorkPattern/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Business.Repository.IRepository.IGenericRepository;
+
+namespace Business.UnitOfWorkPattern.IUnitOfWorkPattern
+
+{
+    // We will also use the 'Unit of Work' framework on top of our 'Generic Repository' pattern.
+    // This interface will function as a 'ledger' for our generic repository classes, which
+    // for the moment are the 'BnB', 'Room', 'Amenity', 'Contact' and the 'Image' datamodel classes.
+
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<BnB> BnBRepository { get; }
+
+        IGenericRepository<Room> RoomRepository { get; }
+
+        IGenericRepository<Amenity> AmenityRepository { get; }
+
+        IGenericRepository<Contact> ContactRepository { get; }
+
+        IGenericRepository<Image> ImageRepository { get; }
+
+        Task Save();         // Before this method all the changes will only be staged and not affected yet.
+    }
+}
