f8db38cfa30db173077fa6adf2abd7f780dc754b
Bart Van Gucht <bartvangucht@dutchmail.com>
2021-05-24 14:29:02 +0200

    Implementing A&A: 1/3 finished

---
+++ BNBCheckInServer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BNBCheckInServer.Areas.Identity.IdentityHostingStartup))]
+namespace BNBCheckInServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
