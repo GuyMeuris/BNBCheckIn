d3a28df993f4ec78a22b05b79b9d23d2edd8ac02
Guy Meuris <guy@vises.be>
2021-05-26 13:42:04 +0200

    Sprint 4: Blazor Server app: bug fixes in 'Authorize' functionality

--- BNBCheckInServer/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BNBCheckInServer/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace BNBCheckInServer.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Registratie onthouden?")]
             public bool RememberMe { get; set; }
         }
 
