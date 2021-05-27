e4f41774bebf7d12dd635d34718c0246d04743fe
Bart Van Gucht <bartvangucht@dutchmail.com>
2021-05-27 20:57:07 +0200

    10 .NET.API: 5 finished.

---
+++ BnBCheckIn_Api/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BnBCheckIn_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<User> _roleManager;
+
+        public AccountController(SignInManager<User> _signInManager, UserManager<User> _userManager, RoleManager<User> _roleManager)
+        {
+            this._signInManager = _signInManager;
+            this._userManager = _userManager;
+            this._roleManager = _roleManager;
+        }
+        
+    }
+}
