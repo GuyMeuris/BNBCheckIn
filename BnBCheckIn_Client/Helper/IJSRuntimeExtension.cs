122b2b2be9eb38ef65ff81b0f8a05a06e1a7b9b9
Guy Meuris <guy@vises.be>
2021-05-31 10:11:38 +0200

    Sprint 5: WASM: Setup

---
+++ BnBCheckIn_Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BNBCheckIn_Client.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask SweetAlertSuccess(this IJSRuntime jsRuntime, string message )
+        {
+            await jsRuntime.InvokeVoidAsync("Window.ShowSwal", "success", message);
+        }
+
+        public static async ValueTask SweetAlertError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("Window.ShowSwal", "error", message);
+        }
+    }
+}
