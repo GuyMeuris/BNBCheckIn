f732e5816d2d4bdf53cf01174c049350868be949
Guy Meuris <guy@vises.be>
2021-05-31 21:38:59 +0200

    WASM: Added Validation annotations to HomeVM

--- BnBCheckIn_Client/Model/ViewModel/HomeVM.cs
+++ BnBCheckIn_Client/Model/ViewModel/HomeVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace BnBCheckIn_Client.Model.ViewModel
     public class HomeVM
     {
         [Required(ErrorMessage = "Startdatum is een verplicht veld.")]
+        [DataType(DataType.DateTime, ErrorMessage = "Startdatum moet correct worden ingegeven.")]
+        [ValidDateRange(ErrorMessage = "Startdatum mag niet vroeger zijn dan vandaag.")]
         public DateTime StartDate { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "Einddatum is een verplicht veld.")]
